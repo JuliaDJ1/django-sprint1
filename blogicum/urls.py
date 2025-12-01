1c0c5f1bce4cbffd4ca1ad2422071e6843645bd8
Вадим Гусейнов <edvard.najt@yandex.ru>
2025-12-02 00:25:15 +0300

    001

--- blogicum/urls.py
+++ blogicum/urls.py
@@ -1,10 +1,9 @@
-from django.urls import path
-from . import views
 
-app_name = 'blog'
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('posts/<int:pk>/', views.post_detail, name='post_detail'),
-    path('category/<slug:category_slug>/', views.category_posts, name='category_posts'),
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
+    path('pages/', include('pages.urls')),
 ]
